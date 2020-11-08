edb5d705c31e013fe2fe75a59290237897b9a250
Kevin Killman <kevinkillman@gmail.com>
2020-11-08 16:30:53 -0500

    weird stuff is happening with my sql

--- data_from_web/QuickDBD-export.sql
+++ data_from_web/QuickDBD-export.sql
@@ -13,14 +13,15 @@ CREATE TABLE "Games" (
     "loser_id" INT   NOT NULL,
     "home_team_id" INT   NOT NULL,
     "away_team_id" INT   NOT NULL,
+    "year" INT   NOT NULL,
     "date" DATE   NOT NULL,
     "stadium_id" INT   NOT NULL,
     "station_id" INT   NOT NULL,
-    "rain" FLOAT   ,
-    "temp" FLOAT   ,
-    "total_pts" INT,
-    "total_yds" INT
-    CONSTRAINT "pk_Game" PRIMARY KEY (
+    "rain" FLOAT   NULL,
+    "temp" FLOAT   NULL,
+    "total_pts" INT   NULL,
+    "total_yds" INT   NULL,
+    CONSTRAINT "pk_Games" PRIMARY KEY (
         "game_id"
      )
 );
@@ -29,9 +30,9 @@ CREATE TABLE "Stadiums" (
     "stadium_id" INT   NOT NULL,
     "name" VARCHAR(255)   NOT NULL,
     "team_id" INT   NOT NULL,
-    "geo_point" VARCHAR(255)   ,
-    "roof_type" VARCHAR(255)   ,
-    "station_id" INT   ,
+    "geo_point" VARCHAR(255)   NULL,
+    "roof_type" VARCHAR(255)   NULL,
+    "station_id" INT   NOT NULL,
     CONSTRAINT "pk_Stadiums" PRIMARY KEY (
         "stadium_id"
      )
@@ -41,10 +42,10 @@ CREATE TABLE "Stations" (
     "station_id" INT   NOT NULL,
     "ghcnd" VARCHAR(255)   NOT NULL,
     "name" VARCHAR(255)   NOT NULL,
-    "country" VARCHAR(255)   NOT NULL,
-    "st" VARCHAR(255)   NOT NULL,
-    "lat" FLOAT   NOT NULL,
-    "lon" FLOAT   NOT NULL,
+    "country" VARCHAR(255)   NULL,
+    "st" VARCHAR(255)   NULL,
+    "lat" FLOAT   NULL,
+    "lon" FLOAT   NULL,
     CONSTRAINT "pk_Stations" PRIMARY KEY (
         "station_id"
      )
@@ -53,24 +54,39 @@ CREATE TABLE "Stations" (
 CREATE TABLE "Teams" (
     "team_id" INT   NOT NULL,
     "team_name" VARCHAR(255)   NOT NULL,
-    "team_conference" VARCHAR(255)   ,
-    "team_division" VARCHAR(255)   ,
+    "team_conference" VARCHAR(255)   NULL,
+    "team_division" VARCHAR(255)   NULL,
     CONSTRAINT "pk_Teams" PRIMARY KEY (
         "team_id"
      )
 );
 
-ALTER TABLE "Games" ADD CONSTRAINT "fk_Games_winner_id" FOREIGN KEY("winner_id")
-REFERENCES "Teams" ("team_id");
+CREATE TABLE "Seasons_Team_Stats" (
+    "team_id" INT   NOT NULL,
+    "year" INT   NOT NULL,
+    "Tm" VARCHAR(255)   NOT NULL,
+    "W" INT   NOT NULL,
+    "L" INT   NOT NULL,
+    "T" INT   NOT NULL,
+    "PF" INT   NOT NULL,
+    "PA" INT   NOT NULL,
+    "PD" INT   NOT NULL,
+    CONSTRAINT "pk_Seasons_Team_Stats" PRIMARY KEY (
+        "team_id","year"
+     )
+);
+
+ALTER TABLE "Games" ADD CONSTRAINT "fk_Games_winner_id_year" FOREIGN KEY("winner_id", "year")
+REFERENCES "Seasons_Team_Stats" ("team_id", "year");
 
 ALTER TABLE "Games" ADD CONSTRAINT "fk_Games_loser_id" FOREIGN KEY("loser_id")
-REFERENCES "Teams" ("team_id");
+REFERENCES "Seasons_Team_Stats" ("team_id");
 
 ALTER TABLE "Games" ADD CONSTRAINT "fk_Games_home_team_id" FOREIGN KEY("home_team_id")
-REFERENCES "Teams" ("team_id");
+REFERENCES "Seasons_Team_Stats" ("team_id");
 
 ALTER TABLE "Games" ADD CONSTRAINT "fk_Games_away_team_id" FOREIGN KEY("away_team_id")
-REFERENCES "Teams" ("team_id");
+REFERENCES "Seasons_Team_Stats" ("team_id");
 
 ALTER TABLE "Games" ADD CONSTRAINT "fk_Games_stadium_id" FOREIGN KEY("stadium_id")
 REFERENCES "Stadiums" ("stadium_id");
@@ -83,4 +99,7 @@ REFERENCES "Teams" ("team_id");
 
 ALTER TABLE "Stadiums" ADD CONSTRAINT "fk_Stadiums_station_id" FOREIGN KEY("station_id")
 REFERENCES "Stations" ("station_id");
+
+ALTER TABLE "Seasons_Team_Stats" ADD CONSTRAINT "fk_Seasons_Team_Stats_team_id" FOREIGN KEY("team_id")
+REFERENCES "Teams" ("team_id");
 
